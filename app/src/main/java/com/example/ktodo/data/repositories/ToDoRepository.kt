de9340f485ef595520a817aeff2793a3b7faeb27
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-04-12 05:40:41 +0330

    add todoRepository

---
+++ app/src/main/java/com/example/ktodo/data/repositories/ToDoRepository.kt
@@ -0,0 +1,37 @@
+package com.example.ktodo.data.repositories
+
+import ToDoDAO
+import com.example.ktodo.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class ToDoRepository @Inject constructor(private val toDoDAO: ToDoDAO) {
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDAO.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDAO.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDAO.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDAO.getSelectedTask(taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDAO.addTask(toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDAO.updateTask(toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDAO.deleteTask(toDoTask)
+    }
+
+    suspend fun deleteAllTasks() {
+        toDoDAO.deleteAllTask()
+    }
+
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>> {
+        return toDoDAO.searchDatabase(searchQuery)
+    }
+}
